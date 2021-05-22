e0844c38e7a994b95dc7d3aed03137c884949ef8
aker-h <82284797+aker-h@users.noreply.github.com>
2021-05-22 23:48:05 +0900

    Update copy-box.js

--- bookmarklet/js/copy-box.js
+++ bookmarklet/js/copy-box.js
@@ -1,6 +1,6 @@
 javascript:(
 	function(){
-		alert('Hello World!\nWritten by JavaScript!\nVersion alpha');
+		console.log('Hello World!\nWritten by JavaScript!\nVersion alpha');
 		
 		var tablearea = document.getElementById("table1");
 		tablearea.select();
