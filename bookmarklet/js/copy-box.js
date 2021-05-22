ee6d849af18b9d48a6745a7a472e13aefa8930e5
aker-h <82284797+aker-h@users.noreply.github.com>
2021-05-22 23:41:44 +0900

    Update copy-box.js

--- bookmarklet/js/copy-box.js
+++ bookmarklet/js/copy-box.js
@@ -1,5 +1,9 @@
 javascript:(
 	function(){
 		alert('Hello World!\nWritten by JavaScript!\nVersion alpha');
+		
+		var tablearea = document.getElementById("table1");
+		tablearea.select();
+		document.execCommand("copy");
 	}
 )()
