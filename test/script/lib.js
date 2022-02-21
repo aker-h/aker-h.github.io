31afeedc96c722b647236fcde71f38b89a8d5654
aker-h <82284797+aker-h@users.noreply.github.com>
2022-02-21 18:07:45 +0900

    Create lib.js

---
+++ test/script/lib.js
@@ -0,0 +1,28 @@
+function applyClickListener () {
+    console.log(document.cookie);
+    
+    $('#testButton').on('click', () => {
+        let themeFlag = document.cookie.split('; ').find ((row) => {
+                                                row.startsWith('themeFlag')}
+                                             )
+                                       .split('=')[1];
+
+        let LightThemeHref = 'https://aker-h.github.io/myLib/css/rootTwitterLight.css',
+            DarkBlueThemeHref = 'https://aker-h.github.io/myLib/css/rootTwitterDarkBlue.css';
+
+            let rootCss = document.getElementById('rootCss');
+
+        switch (themeFlag) {
+            case 'light': {
+                rootCss.href = DarkBlueThemeHref;
+                document.cookie = 'themeFlag: darkBlue';
+                break;
+            }
+            case "darkBlue": {
+                rootCss.href = LightThemeHref;
+                document.cookie = 'themeFlag: light';
+                break;
+            }
+        }
+    });
+};
