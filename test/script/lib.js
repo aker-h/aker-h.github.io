1676a33b2315397b7ef0790fc2f4a8664dd18ba9
aker-h <82284797+aker-h@users.noreply.github.com>
2022-02-21 18:38:34 +0900

    Update lib.js

--- test/script/lib.js
+++ test/script/lib.js
@@ -9,7 +9,14 @@ function applyClickListener () {
         if (debug) {
             console.log(cookies);
         }
-        let themeFlag = cookies.find((row) => {row.startsWith('themeFlag')});
+        let themeFlag = cookies.find((row) => {
+            let strRow = `${row}`;
+            if (strRow.indexOf('themeFlag') !== -1) {
+                return true;
+            } else {
+                return false;
+            }
+        });
         if (debug) {
             console.log(themeFlag);
         }
