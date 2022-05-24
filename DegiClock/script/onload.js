234b0ce3dc9d23ffced2b90b2913344e68d1e2e7
aker-h <b_fox-ran@outlook.jp>
2022-05-25 06:29:19 +0900

    complete

--- DegiClock/script/onload.js
+++ DegiClock/script/onload.js
@@ -2,7 +2,9 @@ $(async() => {
     let hourLeft     = document.getElementById('hoursLeft'),
         hourRight    = document.getElementById('hoursRight'),
         minutesLeft  = document.getElementById('minutesLeft'),
-        minutesRight = document.getElementById('minutesRight');
+        minutesRight = document.getElementById('minutesRight'),
+        secondsLeft  = document.getElementById('secondsLeft'),
+        secondsRight = document.getElementById('secondsRight');
     
     const fix = (num) => {
         return (`0${num}`).slice(-2);
@@ -20,19 +22,24 @@ $(async() => {
         let now = new Date();
 
         let hours = fix(now.getHours()),
-            minutes = fix(now.getMinutes());
+            minutes = fix(now.getMinutes()),
+            seconds = fix(now.getSeconds());
         
         let hLeft  = hours.charAt(0),
             hRight = hours.charAt(1),
             mLeft  = minutes.charAt(0),
-            mRight = minutes.charAt(1);
+            mRight = minutes.charAt(1),
+            sLeft  = seconds.charAt(0),
+            sRight = seconds.charAt(1);
 
-        hourLeft.className = `hours-inner left outer${hLeft}`;
-        hourRight.className = `hours-inner right outer${hRight}`;
-        minutesLeft.className = `minutes-inner left outer${mLeft}`;
+        hourLeft.className     = `hours-inner left outer${hLeft}`;
+        hourRight.className    = `hours-inner right outer${hRight}`;
+        minutesLeft.className  = `minutes-inner left outer${mLeft}`;
         minutesRight.className = `minutes-inner right outer${mRight}`;
+        secondsLeft.className  = `seconds-inner left outer${sLeft}`;
+        secondsRight.className = `seconods-inner right outer${sRight}`;
 
-        console.log(`${hours} : ${minutes}`);
+        console.log(`${hours} : ${minutes} : ${seconds}`);
     }
 
     const loop = () => {
